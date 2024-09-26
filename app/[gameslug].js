747ffabb257a6f553e29adaeaaf2be2ce6e3f4e9
Percy Che <30966796+plche@users.noreply.github.com>
2024-09-26 01:19:46 -0500

    feat(core): add grab and show information for a game

--- app/[gameslug].js
+++ app/[gameslug].js
@@ -1,10 +1,19 @@
-import {View, Text} from "react-native";
-import {Link, Stack, useLocalSearchParams} from "expo-router";
+import {View, Text, ActivityIndicator, ScrollView, Image} from "react-native";
+import {Stack, useLocalSearchParams} from "expo-router";
 import {Screen} from "../components/Screen";
-import {Logo} from "../components/Logo";
+import {useEffect, useState} from "react";
+import {getGameDetails} from "../lib/metacritic";
+import {Score} from "../components/Score";
 
 export default function Detail() {
   const { gameslug } = useLocalSearchParams();
+  const [gameInfo, setGameInfo] = useState(null);
+
+  useEffect(() => {
+    if (gameslug) {
+      getGameDetails(gameslug).then(setGameInfo);
+    }
+  }, [gameslug]);
 
   return (
     <Screen>
@@ -18,12 +27,26 @@ export default function Detail() {
         }}
       />
       <View>
-        <Text className="text-white font-bold mb-8 text-2xl">
-          Detalle del juego {gameslug}
-        </Text>
-        <Link href="/" className="text-blue-500">
-          Volver atrás
-        </Link>
+        {gameInfo === null ? (
+          <ActivityIndicator color={"#fff"} size={"large"} />
+        ) : (
+          <ScrollView>
+            <View className="justify-center items-center text-center">
+              <Image
+                className="mb-4 rounded"
+                source={{uri: gameInfo.img}}
+                style={{width: 214, height: 294}}
+              />
+              <Score score={gameInfo.score} maxScore={100} />
+              <Text className="text-white text-center font-bold text-xl">
+                {gameInfo.title}
+              </Text>
+              <Text className="text-white/70 mt-4 text-left mb-8 text-base">
+                {gameInfo.description}
+              </Text>
+            </View>
+          </ScrollView>
+        )}
       </View>
     </Screen>
   );
